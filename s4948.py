da2d5932b7632704ea0d2995e42ccd138875de4b
TaeYupNoh <107118671+TaeYupNoh@users.noreply.github.com>
2022-07-16 00:01:04 +0900

    4948.py

--- s4948.py
+++ s4948.py
@@ -1,12 +1,16 @@
 # 4948 베르트랑 공준
 n=[]
-while 0 not in n:
+while True:
     fir=int(input())
+    if fir == 0:
+        break
     n.append(fir)
     check = [True for i in range(2*fir+1)]
+    cnt=0
     for i in range(2, 2*fir+1):
         if check[i] == True:
             for j in range(i*2 , 2*fir+1, i):
                 check[j] = False
             if fir < i <= 2*fir :
-                print(i)
+                cnt+=1
+    print(cnt)
