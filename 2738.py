7c942d340cc03e4faac95767fff273ad4c1871e6
TaeYupNoh <107118671+TaeYupNoh@users.noreply.github.com>
2022-07-24 00:49:21 +0900

    2738 행렬 덧셈

---
+++ 2738.py
@@ -0,0 +1,12 @@
+# 2738 행렬 덧셈
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+b = [list(map(int, input().split())) for _ in range(n)]
+
+c = [[0]*m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        c[i][j] = a[i][j]+b[i][j]
+
+for i in range(n):
+    print(*c[i])
