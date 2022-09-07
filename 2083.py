84a9451262aa6c22a8545597c0cf8c5c543b449a
TaeYupNoh <107118671+TaeYupNoh@users.noreply.github.com>
2022-09-07 23:16:08 +0900

    2083 럭비 클럽

---
+++ 2083.py
@@ -0,0 +1,10 @@
+# 2083 럭비 클럽
+name = ''
+while True:
+    name, age, weight = input().split()
+    if name == '#':
+        break
+    if int(age) > 17 or int(weight) >= 80:
+        print(name, 'Senior')
+    else:
+        print(name, 'Junior')
