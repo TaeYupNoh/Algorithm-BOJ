88939f9f24a7293a9a1a0e796a7478a4495024b2
TaeYupNoh <107118671+TaeYupNoh@users.noreply.github.com>
2022-09-24 23:59:19 +0900

    1264 모음의 개수

---
+++ 1264.py
@@ -0,0 +1,13 @@
+# 1264 모음의 개수
+
+word = ['a','e','i','o','u']
+while True:
+    cnt = 0
+    words = input()
+    if words == '#':
+        break
+    for temp in words:
+        if temp in word:
+            cnt+=1
+
+    print(cnt)
