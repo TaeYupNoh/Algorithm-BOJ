e888d5bc7ff1399fa508b4f6de81d322bbe33932
TaeYupNoh <107118671+TaeYupNoh@users.noreply.github.com>
2022-10-03 23:39:09 +0900

    10026_2 적록색약

--- 10026.py
+++ 10026.py
@@ -1,50 +1,51 @@
 # 10026 적록색약
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-lis = [input() for _ in range(n)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+color = [input() for _ in range(n)]
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 
 
-def bfs(a, b):
-    if visited[a][b] == True:
+def bfs(a,b,visit,graph):
+    if visit[a][b] == 1:
         return 0
-
     else:
-        temp = lis[a][b]
-        queue = deque([(a, b)])
-        visited[a][b] = True
+        start = graph[a][b]
+        visit[a][b] = 1
+        queue = deque([(a,b)]) 
         while queue:
-            x, y = queue.popleft()
-
+            x,y = queue.pop()
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-
-                if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False and lis[nx][ny] == temp:
-                    queue.append((nx, ny))
-                    visited[nx][ny] = True
+                if 0<= nx < n and 0<= ny < n and visit[nx][ny] == 0 and graph[nx][ny] == start:
+                    queue.append((nx,ny))
+                    visit[nx][ny] = 1
         return 1
 
 
-# 일반인
-visited = [[False for _ in range(n)] for _ in range(n)]
+# 정상
+nor_visited = [[0 for _ in range(n)] for _ in range(n)]
+
 cnt_1 = 0
 for i in range(n):
     for j in range(n):
-        cnt_1 += bfs(i, j)
+        cnt_1 += bfs(i,j,nor_visited,color)
 
-# 적록색약
-visited = [[False for _ in range(n)] for _ in range(n)]
+# 색약
+nongreen_visited = [[0 for _ in range(n)] for _ in range(n)]
+nongreen_color = color[:]
+
 for i in range(n):
-    if 'G' in lis[i]:
-        lis[i] = lis[i].replace('G', 'R')
+    if 'G' in nongreen_color[i]:
+        nongreen_color[i] = nongreen_color[i].replace('G','R')
 
 cnt_2 = 0
 for i in range(n):
     for j in range(n):
-        cnt_2 += bfs(i, j)
+        cnt_2 += bfs(i,j,nongreen_visited,nongreen_color)
 
-
-print(cnt_1, cnt_2)
+print(cnt_1,cnt_2)
